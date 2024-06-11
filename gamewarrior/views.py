fbd855edd3feec906968bf0bb141ff292484da88
theKywer <kovalenko1104@ya.ru>
2024-06-11 08:06:47 +0500

    небольшая доработка

--- gamewarrior/views.py
+++ gamewarrior/views.py
@@ -3,11 +3,14 @@ from django.shortcuts import render
 from .models import Game
 
 
+
+
 def card(request, id):
     game = Game.objects.get(pk=id)
     return render(request, "card.html", {"model": game})
     # return HttpResponse(id)
 
 def index(request):
-    return render(request, "index.html")
+    ctx = {}
+    return render(request, "index.html", ctx)
     # return HttpResponse(id)
