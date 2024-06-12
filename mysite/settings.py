8edec893c3b2f6278189ec425dcf80a6b4a1c7fe
kano2711 <kovalenko580@yandex.ru>
2024-06-12 22:00:41 +0400

    add style

--- mysite/settings.py
+++ mysite/settings.py
@@ -74,14 +74,21 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'USER': 'kywer',
+#         'PASSWORD': '123QWE',
+#         'NAME': 'bd_warrior',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'USER': 'kywer',
-        'PASSWORD': '123QWE',
-        'NAME': 'bd_warrior',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
