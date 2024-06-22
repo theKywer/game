b73892eccea298bd55af479eaff0766dac4286f4
theKywer <kovalenko1104@ya.ru>
2024-06-22 20:36:13 +0500

    basket

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "ecommerce"
 
 urlpatterns = [
-    path('', views.bascket, name='bascet'),
+    path('', views.basket, name='basket'),
     # path('add/', views.logout_user, name='add'),
     # path('delete/', views.register, name='delete'),
     # path('pay/', views.register, name='pay'),
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 from ecommerce.models import Order, OrderList
 
-def bascket(request):
+def basket(request):
     model = Order.objects.get_or_create(id_user=request.user)
     return render(request, "basket.html", {"model": model}) 
